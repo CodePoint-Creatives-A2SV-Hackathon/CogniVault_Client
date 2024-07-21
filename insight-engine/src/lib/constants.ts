5749d9390c841c8ccbf3e9e3c90a6107da4bff2d
Yoftahe Dereje <107554977+Yophi123@users.noreply.github.com>
2024-07-21 20:27:08 +0300

    Landing page

---
+++ insight-engine/src/lib/constants.ts
@@ -0,0 +1,64 @@
+export const clients = [...new Array(6)].map((client, index) => ({
+    href: `/${index + 1}.png`,
+  }))
+
+export const products = [
+    {
+      title: 'Moonbeam',
+      link: 'https://gomoonbeam.com',
+      thumbnail: '/p01.png',
+    },
+    {
+      title: 'Cursor',
+      link: 'https://cursor.so',
+      thumbnail: '/p02.png',
+    },
+    {
+      title: 'Rogue',
+      link: 'https://userogue.com',
+      thumbnail: '/p03.png',
+    },
+  
+    {
+      title: 'Editorially',
+      link: 'https://editorially.org',
+      thumbnail: '/p04.png',
+    },
+    {
+        title: 'Editrix AI',
+        link: 'https://editrix.ai',
+        thumbnail: '/p05.png',
+      },
+      {
+        title: 'Pixel Perfect',
+        link: 'https://app.pixelperfect.quest',
+        thumbnail: '/p06.png',
+      },
+    
+      {
+        title: 'Algochurn',
+        link: 'https://algochurn.com',
+        thumbnail: '/p01.png',
+      },
+      {
+        title: 'Aceternity UI',
+        link: 'https://ui.aceternity.com',
+        thumbnail: '/p02.png',
+      },
+      {
+        title: 'Tailwind Master Kit',
+        link: 'https://tailwindmasterkit.com',
+        thumbnail: '/p03.png',
+      },
+      {
+        title: 'SmartBridge',
+        link: 'https://smartbridgetech.com',
+        thumbnail: '/p04.png',
+      },
+      {
+        title: 'Renderwork Studio',
+        link: 'https://renderwork.studio',
+        thumbnail: '/p05.png',
+      },
+    
+]
